f3d058b5b9dcaafe7f15933613f7e71e5e68ea66
Arunava Dev <arunavadasgupta1996@gmail.com>
2026-06-12 02:41:27 +0530

    feat: set default Guid Id on User entity

--- MyVersion10Api/Entities/User.cs
+++ MyVersion10Api/Entities/User.cs
@@ -10,7 +10,7 @@ namespace MyVersion10Api.Entities
     public class User
     {
         [Key]
-        public Guid Id { get; set; }
+        public Guid Id { get; set; } = Guid.NewGuid();
         public string? Name { get; set; }
         public string? Email { get; set; }
         public string? Password { get; set; }
