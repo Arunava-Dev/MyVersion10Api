9cf73cb1cd671f3a6ee4f4f60b786daa667c49e9
Arunava Dev <arunavadasgupta1996@gmail.com>
2026-06-12 23:41:52 +0530

    feat: add Employee entity class

---
+++ MyVersion10Api/Entities/Employee.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyVersion10Api.Entities
+{
+    public class Employee
+    {
+        [Key]
+        public Guid Id { get; set; }
+        public string? Name { get; set; }
+        public DateTime? CreatedDate { get; set; }
+        public DateTime? LastModifiedDate { get; set; }
+        public DateOnly? DOB { get; set; }
+        public string? Position { get; set; }
+        public string? Department { get; set; }
+        public string? EmailAddress { get; set; }
+    }
+}
