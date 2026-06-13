e6fe45ff3ee8947b64e7dac79eba85e1c9aae084
Arunava Dev <arunavadasgupta1996@gmail.com>
2026-06-14 03:12:40 +0530

    feat: register AppDbContext with SQL Server connection string

--- MyVersion10Api/Program.cs
+++ MyVersion10Api/Program.cs
@@ -1,6 +1,12 @@
+using Microsoft.EntityFrameworkCore;
+using MyVersion10Api.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+
+builder.Services.AddDbContext<AppDbContext>
+    (options => options.UseSqlServer(builder.Configuration.GetConnectionString("MyConnection")));
 
 builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
