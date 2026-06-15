b2eee341c1d60a3a183963965f5ac6623f71aa6a
Arunava Dev <arunavadasgupta1996@gmail.com>
2026-06-16 00:23:41 +0530

    feat : Add UserDto

---
+++ MyVersion10Api/DTO/UserDto.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyVersion10Api.DTO
+{
+    public class UserDto
+    {
+        [Key]
+        public Guid Id { get; set; } = Guid.NewGuid();
+        public string? Name { get; set; }
+        public string? Email { get; set; }
+        public string? Password { get; set; }
+    }
+}
