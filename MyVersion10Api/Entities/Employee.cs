9ddc8c62f137e1fb56dc3f4b01572a7b438abb75
Arunava Dev <arunavadasgupta1996@gmail.com>
2026-06-16 00:22:55 +0530

    feat: set default Guid Id on Employee entity

--- MyVersion10Api/Entities/Employee.cs
+++ MyVersion10Api/Entities/Employee.cs
@@ -10,7 +10,7 @@ namespace MyVersion10Api.Entities
     public class Employee
     {
         [Key]
-        public Guid Id { get; set; }
+        public Guid Id { get; set; } = Guid.NewGuid();
         public string? Name { get; set; }
         public DateTime? CreatedDate { get; set; }
         public DateTime? LastModifiedDate { get; set; }
